def0b8944f800137ad465721f908a12860728055
Conceal34 <vinnerhooda@gmail.com>
2025-09-21 00:35:49 +0530

    tryin

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -24,49 +24,49 @@ const blogs =
     ]
 
 
-type PageProps = {
-    params: Promise<{ id: string }>
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
+// type PageProps = {
+//     params: Promise<{ id: string }>
+//     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+// }
 
-export default async function BlogPage({ params }: PageProps) {
+export default async function BlogPage({ params }: any) {
 
     const { id } = await params;
-    const blogId = parseInt(id);
-    const blog = blogs.find(blog => blog.id === blogId);
+    // const blogId = parseInt(id);
+    // const blog = blogs.find(blog => blog.id === blogId);
+    return <div>{id}</div>
+    // if (!blog) {
+    //     notFound();
+    // }
 
-    if (!blog) {
-        notFound();
-    }
+    // return (
+    //     <div className="flex flex-col gap-8 items-center justify-center w-5xl">
+    //         <div className="flex flex-col gap-8 mt-10 justify-center items-center  bg-gray-200 rounded-2xl p-12">
+    //             <div>
+    //                 <h1 className="text-5xl text-amber-600">{blog.title}</h1>
+    //             </div>
+    //             <div className="text-xl/relaxed">
+    //                 <p className="text-right text-gray-500 mb-2">{blog.posted_on}</p>
+    //                 {blog.post}
+    //             </div>
+    //         </div>
+    //         <div className="grid grid-cols-2 gap-4" >
+    //             {blogs
+    //                 .filter((sampleBlog) => sampleBlog.id != blog.id)
+    //                 .slice(0,2)
+    //                 .map((relatedBlogs) => {
+    //                     return(
+    //                         <Link href={`/blog/${relatedBlogs.id}`} key={relatedBlogs.id}
+    //                             className="border-1 border-amber-400 rounded-2xl p-4 flex flex-col gap-2 shadow-lg hover:shadow-2xl transition-shadow duration-200"
+    //                         >
+    //                             <h3 className="text-2xl text-amber-600">{relatedBlogs.title}</h3>
+    //                             <p className="text-md">{relatedBlogs.post.length > 150 ? `${relatedBlogs.post.slice(0,150)}...` : relatedBlogs.post}</p>
+    //                         </Link>
+    //                     )
+    //                 })
+    //             }
+    //         </div>
 
-    return (
-        <div className="flex flex-col gap-8 items-center justify-center w-5xl">
-            <div className="flex flex-col gap-8 mt-10 justify-center items-center  bg-gray-200 rounded-2xl p-12">
-                <div>
-                    <h1 className="text-5xl text-amber-600">{blog.title}</h1>
-                </div>
-                <div className="text-xl/relaxed">
-                    <p className="text-right text-gray-500 mb-2">{blog.posted_on}</p>
-                    {blog.post}
-                </div>
-            </div>
-            <div className="grid grid-cols-2 gap-4" >
-                {blogs
-                    .filter((sampleBlog) => sampleBlog.id != blog.id)
-                    .slice(0,2)
-                    .map((relatedBlogs) => {
-                        return(
-                            <Link href={`/blog/${relatedBlogs.id}`} key={relatedBlogs.id}
-                                className="border-1 border-amber-400 rounded-2xl p-4 flex flex-col gap-2 shadow-lg hover:shadow-2xl transition-shadow duration-200"
-                            >
-                                <h3 className="text-2xl text-amber-600">{relatedBlogs.title}</h3>
-                                <p className="text-md">{relatedBlogs.post.length > 150 ? `${relatedBlogs.post.slice(0,150)}...` : relatedBlogs.post}</p>
-                            </Link>
-                        )
-                    })
-                }
-            </div>
-
-        </div>
-    )
+    //     </div>
+    // )
 }
